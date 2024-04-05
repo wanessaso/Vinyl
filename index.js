260fa847a2cd083778426144fea094a34818e157
wanessaso <164978040+wanessaso@users.noreply.github.com>
2024-04-05 13:28:03 -0600

    Updated reference Id for description element

--- index.js
+++ index.js
@@ -2,13 +2,12 @@ function refreshWeather(response) {
   let temperatureElement = document.querySelector("#temperature");
   let temperature = response.data.temperature.current;
   let cityElement = document.querySelector("#city");
-  let descriptionElement = document.querySelector("#description");
+  let descriptionElement = document.querySelector(".description");
   let humidityElement = document.querySelector("#humidity");
   let windSpeedElement = document.querySelector("#wind-speed");
   let timeElement = document.querySelector("#time");
   let date = new Date(response.data.time * 1000);
   let iconElement = document.querySelector("#icon");
-  icon.innerHTML = `<img src="${response.condition.icon.url}" class="weather-app-icon"/>`;
 
   cityElement.innerHTML = response.data.city;
   timeElement.innerHTML = formatDate(date);
