e86d62ead0e4538a3ec46e8f73474a3807853624
wanessaso <164978040+wanessaso@users.noreply.github.com>
2024-04-04 22:49:01 -0600

    Update index.js

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ function formatDate(date) {
 }
 
 function searchCity(city) {
-  let apiKey = "b2a5adcct04b33178913oc335f405433";
+  let apiKey = "3f370f2fcfc8ab8924dbf44a3bcato1c";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
   axios.get(apiUrl).then(refreshWeather);
 }
