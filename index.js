cfd5c7001a29ef124308058f1fc92117ad7f8ec3
wanessaso <164978040+wanessaso@users.noreply.github.com>
2024-04-14 14:10:47 -0600

    Added pngs for forecast images and made them bigger, replaced dummy content with live content from API

--- index.js
+++ index.js
@@ -52,6 +52,10 @@ function handleSearchSubmit(event) {
 
   searchCity(searchInput.value);
 }
+
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+}
 function getForecast(city) {
   let apiKey = "3f370f2fcfc8ab8924dbf44a3bcato1c";
   let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key =${apiKey}&units=imperial`;
@@ -70,21 +74,25 @@ function displayForecast(response) {
   let forecastHtml = "";
   axios(apiUrl).then(displayForecast);
 
-  days.forEach(function (day) {
-    forecastHtml =
-      forecastHtml +
-      `
+  response.data.daily.forEach(function (day, index) {
+    if (index < 5) {
+      forecastHtml += `
           <div class="weather-forecast-day">
-            <div class="weather-forecast-date">${day}</div>
-            <div class="weather-forecast-icon">🌤️</div>
+            <div class="weather-forecast-date">${formatDay(day.dt)}</div>
+            <img src="${
+              day.condition.icon_url
+            }" class="weather-forecast-icon" /> 
             <div class="weather-forecast-temperatures">
               <div class="weather-forecast-temperature">
-                <strong>15º</strong>
+                <strong>${day.temperature.minimum}°</strong>
               </div>
-              <div class="weather-forecast-temperature">9°</div>
+              <div class="weather-forecast-temperature">${Math.round(
+                day.temperature.maximum
+              )}°</div>
             </div>
           </div>
-            `;
+        `;
+    }
   });
   let forecastElement = document.querySelector("#forecast");
   forecastElement.innerHTML = forecastHtml;
